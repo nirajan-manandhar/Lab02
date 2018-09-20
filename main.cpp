3f82a0c0aa670f946cb750cf95327f4f0543c880
nirajan-manandhar <manandnirajan@gmail.com>
2018-09-20 14:55:14 -0700

    Added a Calculate function that uses a vector and some math to find the desired values.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include <fstream>
 #include <random>
 #include <ctime>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cout << "Amazing Chloe" << endl;
-    return 0;
-}
+vector<double> doubleVector;
+
 
 void createFile() {
-    ofstream fout:
+    ofstream fout;
     fout.open("Readings.txt");
 }
 
-int maxInt(){
+int createInt(){
     int maxInt;
 
     random_device rd;
@@ -31,9 +31,69 @@ int maxInt(){
 double createDouble() {
     double currDouble;
 
-    default_random_engine doubleGenerator(time(nullptr));
-    uniform_real_distribution<double> doubleDistribution(50,90);
-    currDouble = doubleDistribution(doubleGenerator);
+    random_device rd;
+    mt19937 doubleGenerator(rd());
+    uniform_real_distribution<> intDistribution(50.000,90.000);
 
-    return currDouble;
+    currDouble = intDistribution(doubleGenerator);
+
+    return currDouble ;
+}
+
+void writeFile(){
+    ofstream fout;
+    fout.open("Readings.txt");
+    int indexMax = createInt();
+    int i{0};
+    while(i < indexMax) {
+        fout << i << " " << setprecision(3)<< fixed << createDouble() << endl;
+        i++;
+    }
+}
+
+void readFile() {
+    string line;
+    double doubleValue{0};
+    int integer{0};
+
+    ifstream fin;
+    fin.open("Readings.txt");
+
+    while (getline(fin, line)) {
+        istringstream iss(line);
+        iss >> integer >> doubleValue;
+        doubleVector.push_back(doubleValue);
+    }
+}
+
+
+void calculate() {
+    int vectorSize = doubleVector.size();
+    double sumOfElements{0};
+    double median = doubleVector.at(vectorSize/2);
+
+    for(double value:doubleVector){
+        sumOfElements += value;
+    }
+    double average = (sumOfElements/(double) vectorSize);
+
+    sort(doubleVector.begin(), doubleVector.end());
+
+
+    cout << "There are " << doubleVector.size() << " readings in the file." << endl;
+    cout << "The average reading is " << average << endl;
+    cout << "The highest reading is " << doubleVector.at(vectorSize - 1) << endl;
+    cout << "THe lowest reading is " << doubleVector.at(0) << endl;
+    cout << "The median reading is " << median << endl;
+}
+
+
+
+
+int main() {
+    createFile();
+    writeFile();
+    readFile();
+    calculate();
+    return 0;
 }
