fda7465cd103cf99b3b063bcdc52badd2663c084
nirajan-manandhar <manandnirajan@gmail.com>
2018-09-20 11:36:00 -0700

    Added functions to make a new file, generate a random integer and a random double within bounds

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -6,4 +9,31 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     cout << "Amazing Chloe" << endl;
     return 0;
+}
+
+void createFile() {
+    ofstream fout:
+    fout.open("Readings.txt");
+}
+
+int maxInt(){
+    int maxInt;
+
+    random_device rd;
+    mt19937 intGenerator(rd());
+    uniform_int_distribution<> intDistribution(512,1024);
+
+    maxInt = intDistribution(intGenerator);
+
+    return maxInt;
+}
+
+double createDouble() {
+    double currDouble;
+
+    default_random_engine doubleGenerator(time(nullptr));
+    uniform_real_distribution<double> doubleDistribution(50,90);
+    currDouble = doubleDistribution(doubleGenerator);
+
+    return currDouble;
 }
